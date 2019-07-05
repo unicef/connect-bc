01e1c5175f3f45358ece6ec5582b22371e9a1980
Mehran Hydary <mehran.hydary@gmail.com>
2019-07-05 14:03:36 -0400

    Added some stuff to src/index

---
+++ users-api/src/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import {
+    deleteUser,
+    getUsers,
+    notFound,
+    postUser,
+    patchUser
+} from './controllers'
+import makeCallback from './express-callback'
+
+dotenv.config()
+
+// const apiRoot = process.env.DM_API_ROOT
+const app = express()
+app.use(bodyParser.json())
+app.use((_, res, next) => {
+    res.set({ Tk: '!'})
+    next()
+})
+// Set roots here for each of the controllers listed above
+if(process.env.DM_ENV === 'dev') {
+    // Listen for requests
+    app.listen(3000, () => {
+        console.log('Server is listening to port 3000')
+    })
+}
+
+export default app
