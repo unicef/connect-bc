4b151746376d4f604df81017f778422c36914770
Mehran Hydary <mehran.hydary@gmail.com>
2020-07-23 22:29:25 -0400

    Adding make files for all microservices

--- regions-blockchain/server.js
+++ regions-blockchain/server.js
@@ -1,24 +1,29 @@
-const express = require("express")
-, mongoose = require("mongoose")
-, bodyParser = require("body-parser")
-, dotenv = require('dotenv')
-, cors = require('cors')
-, app = express()
-, contractCreations = require("./routes/contractCreationRoutes")
-, blockchainRequests = require("./routes/blockchainRequestRoutes")
+const express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  dotenv = require("dotenv"),
+  cors = require("cors"),
+  app = express(),
+  contractCreations = require("./routes/contractCreationRoutes"),
+  blockchainRequests = require("./routes/blockchainRequestRoutes");
 
-dotenv.config()
+dotenv.config();
 
-const db = process.env.MONGO_URI
-, db_name = process.env.DB_NAME
-, port = process.env.PORT
+const db = process.env.MONGO_URI,
+  db_name = process.env.DB_NAME,
+  port = process.env.PORT;
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/api/contract-creations", contractCreations)
-app.use("/api/blockchain-requests", blockchainRequests)
-app.listen(port, () => console.log(`Server for regions blockchain api is up and running on port ${port}`))
-mongoose.connect( db + db_name, { useNewUrlParser: true })
-    .then(() => console.log('Connection to Mongo DB is confirmed on port 27017'))
-    .catch(err => console.log(err))
+app.use("/api/contract-creations", contractCreations);
+app.use("/api/blockchain-requests", blockchainRequests);
+app.listen(port, () =>
+  console.log(
+    `Server for regions blockchain api is up and running on port ${port}`
+  )
+);
+mongoose
+  .connect(db + db_name, { useNewUrlParser: true })
+  .then(() => console.log("Connection to Mongo DB is confirmed on port 27017"))
+  .catch((err) => console.log(err));
